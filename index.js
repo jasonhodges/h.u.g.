b0723e57b2d5ab3923b3e35fc118698ff3ef9731
Jason <jasonlhodges@gmail.com>
2018-06-19 23:41:37 -0400

    fix(index.js): Make bot choose issue by branch name.
    Regex out the issue number based off of branch name.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 module.exports = robot => {
   robot.on('status', async context => {
+      robot.log('Branch Name:', context.payload.branches[0].name)
+    const branchRegex = /([0-9]*\d)/g;
+    const branchNum = branchRegex.exec(context.payload.branches[0].name);
     if (context.payload.state === 'success') {
-      // robot.log('Context State:', context.payload.state)
-      const params = context.issue({number: 5, labels: [{name: "CI:Passed"}]});
+      const params = context.issue({number: branchNum, labels: [{name: "CI:Passed"}]});
       return context.github.issues.edit(params)
     } else if (context.payload.state === 'error' || context.payload.state === 'failed') {
       // robot.log('Context State:', context.payload.state)
-      const params = context.issue({number: 5, labels: [{name: "CI:Failed"}]});
+      const params = context.issue({number: branchNum, labels: [{name: "CI:Failed"}]});
       return context.github.issues.edit(params)
     }
   })
