f8c61494965b0bd0d4cd45cf8657fc0e1441b216
Jason <jasonlhodges@gmail.com>
2018-06-20 09:56:29 -0400

    feat(index.js):
    Filter existing labels and add Passed Failed.

--- index.js
+++ index.js
@@ -1,15 +1,36 @@
+async function getIssueLabels(context, issueNum) {
+  const params = context.issue({number: issueNum});
+  const labels = await context.github.issues.getIssueLabels(params);
+  console.log('---------------------------Labels: ', labels);
+  return labels;
+}
+
 module.exports = robot => {
   robot.on('status', async context => {
-      robot.log('Branch Name:', context.payload.branches[0].name)
+    robot.log('Branch Name:', context.payload.branches[0].name);
     const branchRegex = /([0-9]*\d)/g;
     const branchNum = branchRegex.exec(context.payload.branches[0].name);
     if (context.payload.state === 'success') {
-      const params = context.issue({number: branchNum, labels: [{name: "CI:Passed"}]});
+      const labelParams = context.issue({number: branchNum});
+      const labels = await context.github.issues.getIssueLabels(labelParams);
+      const filteredLabels = labels.data.filter((l) => {
+        return !/CI:/.test(l.name);
+      });
+      robot.log('---------------------------Filtered Labels: ', filteredLabels);
+      const newLabels = [...filteredLabels, {name: "CI:Passed"}];
+      const params = context.issue({number: branchNum, labels: newLabels});
       return context.github.issues.edit(params)
     } else if (context.payload.state === 'error' || context.payload.state === 'failed') {
-      // robot.log('Context State:', context.payload.state)
-      const params = context.issue({number: branchNum, labels: [{name: "CI:Failed"}]});
+      const labelParams = context.issue({number: branchNum});
+      const labels = await context.github.issues.getIssueLabels(labelParams);
+      const filteredLabels = labels.data.filter((l) => {
+        return !/CI:/.test(l.name);
+      });
+      robot.log('---------------------------Filtered Labels: ', filteredLabels);
+      const newLabels = [...filteredLabels, {name: "CI:Failed"}];
+      const params = context.issue({number: branchNum, labels: newLabels});
       return context.github.issues.edit(params)
     }
   })
 };
+
