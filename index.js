ca5df577fa473b489f2b1a992d8ea23a3b0ecbd5
Jason <jasonlhodges@gmail.com>
2018-06-19 23:05:26 -0400

    feat(index.js): Make WaffleSyrup bot update labels in Github

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 module.exports = robot => {
   robot.on('status', async context => {
-    context.log('Status Payload: ', context.payload)
+    if (context.payload.state === 'success') {
+      // robot.log('Context State:', context.payload.state)
+      const params = context.issue({number: 5, labels: [{name: "CI:Passed"}]});
+      return context.github.issues.edit(params)
+    } else if (context.payload.state === 'error' || context.payload.state === 'failed') {
+      // robot.log('Context State:', context.payload.state)
+      const params = context.issue({number: 5, labels: [{name: "CI:Failed"}]});
+      return context.github.issues.edit(params)
+    }
   })
 };
